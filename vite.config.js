0ec441191f326aa039c13f809afe46a321602140
Kumari Laxmi Sharma <laxmi@Kumaris-MacBook-Air.local>
2025-07-26 02:36:30 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rayesh_portfolio/', // Must match your GitHub repo name exactly
+  base: process.env.VITE_BASE_PATH || "/rayesh_portfolio", 
   
-})
+});
